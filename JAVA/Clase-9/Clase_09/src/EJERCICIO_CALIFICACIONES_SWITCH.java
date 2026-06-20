fff3beeeb61500f962cbb0832e70d7c42bed3d38
Jaque Esteban <jaqueesteban2002@gmail.com>
2026-06-19 21:57:40 -0300

    correcciones clase 9

--- JAVA/Clase-9/Clase_09/src/EJERCICIO_CALIFICACIONES_SWITCH.java
+++ JAVA/Clase-9/Clase_09/src/EJERCICIO_CALIFICACIONES_SWITCH.java
@@ -1,9 +1,7 @@
 
 import java.util.Scanner;
 
-/* Ejercicio: Sistema de calificaciones con if else, como tarea hacerlo tambien
-con la sentencia de control Switch*/
-public class EjerSisCalif {
+public class EJERCICIO_CALIFICACIONES_SWITCH {
     public static void main(String[] args) {
         
         Scanner entrada = new Scanner(System.in);
@@ -11,28 +9,7 @@ public class EjerSisCalif {
         System.out.println("Ingrese la nota del 1 a 10: ");
         
         var nota = Integer.parseInt(entrada.nextLine());
-        /*
-        if (nota >= 9 && nota <= 10){
-            System.out.println("Su calificacion es: A");
-        }
-        else if (nota >= 8 && nota <= 9){
-            System.out.println("Su calificacion es: B");
-        }
-        else if (nota >= 7 && nota <= 8){
-            System.out.println("Su calificacion es: C");
-        }
-        else if (nota >= 6 && nota <=7 ){
-            System.out.println("Su calificacion es: D");
-        }
-        else if (nota >= 0 && nota < 6){
-            System.out.println("Su calificacion es: F");
-        }
-        else {
-            System.out.println("Fuera de rango");
-        }
-        */
         
-        //Ahora con la sentencia Switch
         switch(nota){
             case 9: case 10:
                 System.out.println("Su calificación es: A");
